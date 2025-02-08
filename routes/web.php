61b462f2e665a271bb4305edcd7ee8aed29a66ad
David Urbina <davidurbina.dev@gmail.com>
2025-02-08 15:40:11 -0500

    Created signup view

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,6 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 // Singup routes
 Route::get('/signup', function() {
     return Inertia::render('Signup');
