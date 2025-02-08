39b76905c9f57a9e8e8e9167ee8cc3621ba56f01
David Urbina <davidurbina.dev@gmail.com>
2025-02-08 14:42:33 -0500

    Created signup routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Singup routes
+Route::get('/singup', [UserController::class, 'showSignupForm'])->name('signup.form');
+Route::post('/signup', [UserController::class, 'registerUser'])->name('signup.submit');
