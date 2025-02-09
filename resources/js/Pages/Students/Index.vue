ec455333d8be0eba720b61f49096a4617532a4a8
David Urbina <davidurbina.dev@gmail.com>
2025-02-09 14:11:30 -0500

    Fixed missing container

--- resources/js/Pages/Students/Index.vue
+++ resources/js/Pages/Students/Index.vue
@@ -20,98 +20,100 @@ const deleteStudent = (id) => {
 </script>
 
 <template>
-    <!-- Delete form -->
-    <form>
-        <input v-model="deleteForm.id" type="hidden" name="id">
-    </form>
+    <div class="container mx-auto p-4 max-w-4xl">
+        <!-- Delete form -->
+        <form>
+            <input v-model="deleteForm.id" type="hidden" name="id">
+        </form>
 
-    <div class="flex justify-between items-center mb-4">
-        <h1 class="text-xl font-bold">Student List</h1>
+        <div class="flex justify-between items-center mb-4">
+            <h1 class="text-xl font-bold">Student List</h1>
 
-        <SimpleButton
-            text="New student"
-            type="link"
-            href="/students/create"
-            as="button"
-        />
-    </div>
+            <SimpleButton
+                text="New student"
+                type="link"
+                href="/students/create"
+                as="button"
+            />
+        </div>
 
-    <!-- Table to display students -->
-    <table class="min-w-full table-auto border-collapse mb-4">
-        <thead>
-        <tr>
-            <th class="border-b px-4 py-2 text-left">ID</th>
-            <th class="border-b px-4 py-2 text-left">First Name</th>
-            <th class="border-b px-4 py-2 text-left">Last Name</th>
-            <th class="border-b px-4 py-2 text-left">Address</th>
-            <th class="border-b px-4 py-2 text-center">Actions</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr v-for="student in paginator.data" :key="student.id">
-            <td class="border-b px-4 py-2">{{ student.id }}</td>
-            <td class="border-b px-4 py-2">{{ student.first_name }}</td>
-            <td class="border-b px-4 py-2">{{ student.last_name }}</td>
-            <td class="border-b px-4 py-2">{{ student.address }}</td>
-            <td class="border-b px-4 py-2 text-center">
+        <!-- Table to display students -->
+        <table class="min-w-full table-auto border-collapse mb-4">
+            <thead>
+            <tr>
+                <th class="border-b px-4 py-2 text-left">ID</th>
+                <th class="border-b px-4 py-2 text-left">First Name</th>
+                <th class="border-b px-4 py-2 text-left">Last Name</th>
+                <th class="border-b px-4 py-2 text-left">Address</th>
+                <th class="border-b px-4 py-2 text-center">Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="student in paginator.data" :key="student.id">
+                <td class="border-b px-4 py-2">{{ student.id }}</td>
+                <td class="border-b px-4 py-2">{{ student.first_name }}</td>
+                <td class="border-b px-4 py-2">{{ student.last_name }}</td>
+                <td class="border-b px-4 py-2">{{ student.address }}</td>
+                <td class="border-b px-4 py-2 text-center">
+                    <SimpleButton
+                        :href="`/students/${student.id}`"
+                        text="View"
+                        type="link"
+                        color="success"
+                        class="mr-2"
+                    />
+                    <SimpleButton
+                        :href="`/students/${student.id}/edit`"
+                        text="Edit"
+                        type="link"
+                        color="primary"
+                        class="mr-2"
+                    />
+                    <SimpleButton
+                        @click="() => { deleteStudent(student.id) }"
+                        :href="`/students/${student.id}`"
+                        text="Delete"
+                        color="danger"
+                    />
+                </td>
+            </tr>
+            </tbody>
+        </table>
+
+        <!-- Pagination controls -->
+        <div class="flex justify-center mt-4">
+            <span>
                 <SimpleButton
-                    :href="`/students/${student.id}`"
-                    text="View"
+                    v-if="paginator.prev_page_url"
+                    :href="paginator.prev_page_url"
+                    text="&laquo; Previous"
                     type="link"
-                    color="success"
+                    color="muted-light"
                     class="mr-2"
                 />
+            </span>
+
+            <span v-for="(page, index) in paginator.links" :key="index">
                 <SimpleButton
-                    :href="`/students/${student.id}/edit`"
-                    text="Edit"
+                    v-if="page.url && index > 0 && index < (paginator.links.length - 1)"
+                    :href="page.url"
+                    :text="page.label"
+                    :color="page.active ? 'primary' : 'primary-light'"
+                    class="mx-1"
                     type="link"
-                    color="primary"
-                    class="mr-2"
                 />
+            </span>
+
+            <span>
                 <SimpleButton
-                    @click="() => { deleteStudent(student.id) }"
-                    :href="`/students/${student.id}`"
-                    text="Delete"
-                    color="danger"
+                    v-if="paginator.next_page_url"
+                    :href="paginator.next_page_url"
+                    text="Next &raquo;"
+                    type="link"
+                    color="muted-light"
+                    class="ml-2"
                 />
-            </td>
-        </tr>
-        </tbody>
-    </table>
-
-    <!-- Pagination controls -->
-    <div class="flex justify-center mt-4">
-        <span>
-            <SimpleButton
-                v-if="paginator.prev_page_url"
-                :href="paginator.prev_page_url"
-                text="&laquo; Previous"
-                type="link"
-                color="muted-light"
-                class="mr-2"
-            />
-        </span>
-
-        <span v-for="(page, index) in paginator.links" :key="index">
-            <SimpleButton
-                v-if="page.url && index > 0 && index < (paginator.links.length - 1)"
-                :href="page.url"
-                :text="page.label"
-                :color="page.active ? 'primary' : 'primary-light'"
-                class="mx-1"
-                type="link"
-            />
-        </span>
-
-        <span>
-            <SimpleButton
-                v-if="paginator.next_page_url"
-                :href="paginator.next_page_url"
-                text="Next &raquo;"
-                type="link"
-                color="muted-light"
-                class="ml-2"
-            />
-        </span>
+            </span>
+        </div>
     </div>
 </template>
